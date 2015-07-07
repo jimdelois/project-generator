7d00bc2792f5282253548c9e47e23e04bbdca8c9
Jim DeLois <jim@deloisweb.com>
2015-07-07 12:19:56 -0400

    Renamed Base Test Case; Updating Mock Gen

---
+++ assets/component/tests/lib/AbstractTestCase.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace __TOKEN_NAMESPACE__\Test;
+
+class AbstractTestCase extends \PHPUnit_Framework_TestCase {
+
+  protected function _getFullMock( $class_name ) {
+
+    return $this->getMockBuilder( $class_name )
+      ->disableOriginalConstructor()
+      ->getMock();
+  }
+
+}
